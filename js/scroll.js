32a48846f441972b5e9a557ba67bb8a1ed7fb2d8
kamiljankowski2 <45606008+kamiljankowski2@users.noreply.github.com>
2019-06-13 16:49:24 +0200

    Add files via upload

--- js/scroll.js
+++ js/scroll.js
@@ -1,4 +1,4 @@
-$('a.nav-link').on('click', function () {
+$('a.nav-link, a.nav-link-footer').on('click', function () {
     const goToSection = "[data-section=" + $(this).attr('id') + "]";
     $('body, html').animate({
         scrollTop: ($(goToSection).offset().top) - 50 + "px"
